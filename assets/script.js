71026c229f965110ab9249c95d426d8d8f839ff3
Brian Chapman <chapman.brian86@gmail.com>
2022-11-15 20:00:00 -0500

    Put all the questions and answers into variables

--- assets/script.js
+++ assets/script.js
@@ -14,26 +14,53 @@ var startButton = document.getElementById("start-button");
 var timerEl = document.getElementById("timer-text");
 var quizBox = document.getElementById("question-container");
 var questionTitle = document.getElementById("question-title");
+var questionUl = document.getElementById("question-UL");
 var questionOptions = document.getElementById("question-list");
+var time = 60; 
 
-var time = 75; 
+var questions = [
+"It's grilled cheese time! What's the first step in making one?",
+"Nice! We got the bread. What's next?",
+"Cheesin' it up! What do we do next?",
+"Pan or press if prepped! What's the next step?",
+"Now we're grilling! What's the final step?"
+];
+
+var answers = [
+"Eat it!",
+"Get out some bread.",
+"Put the cheese on it",
+"Throw ingredients in a pan or panini press",
+"Check an email",
+"Grease the pan or panini press",
+"Peek at Black Friday deals",
+"Send a 'per my last email' response to your lazy coworker",
+"Throw it in the garbage"
+] 
+
 
 startButton.addEventListener("click", function(){
     
-    quizBox.innerHTML = "";
-    timerStart();
+    questionTitle.innerHTML = "It's grilled cheese time! What's the first step in making one?";
+    var newUl = document.createElement("ul");
+    // var newLi = document.
+
+    var timeAttack = setInterval(function() {
+    time--;
+    timerEl.innerHTML = "Time:" + time;
+
+
+    if(time === 0) {
+    clearInterval(timeAttack);
+    highScoreMessage();
+      
+} }, 1000);
+
 })
 
-function timerStart() {
 
-var timeAttack = setInterval(function() {
-         time--;
-        timerEl.innerHTML = "Time:" + time;
 
-        if(time === 0) {
-        clearInterval(timeAttack);
-        highScoreMessage();
-       } 
-    }, 1000);
-}
+
+
+
 
